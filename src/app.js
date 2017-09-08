1bb9c81883d35c3220abceb36dbe6267cab974ca
KuKKoMaH <kukkoman@gmail.com>
2017-09-09 01:08:58 +0300

    in progress

--- src/app.js
+++ src/app.js
@@ -42,10 +42,55 @@ $('.header__cabinet, .header__button').magnificPopup({
 
 $('input[type="phone"]').mask("+7 (999) 999-99-99");
 
+/**
+ * Анимации блока "КАК ЭТО РАБОТАЕТ"
+ */
+const images = [];
+const animationQueue = [];
+let currentAnimation = null;
+const playNext = () => {
+  const next = animationQueue.shift();
+  if (!next) return;
+  play(next);
+};
+const play = (img, force) => {
+  img.played = true;
+  if (!force && currentAnimation) {
+    animationQueue.push(img);
+    return;
+  }
+  img.$el.find('animate, animateTransform').each((i, animate) => animate.beginElement());
+  if (!force) {
+    currentAnimation = img;
+    setTimeout(() => {
+      currentAnimation = null;
+      playNext();
+    }, 1000);
+  }
+};
 $('.works__image img').each((i, el) => {
   $.get(el.src, (content) => {
     const $img = $(content).find('svg');
     $(el).replaceWith($img);
-    $img.on('click', () => $img.find('animate, animateTransform').each((i, animate) => animate.beginElement()))
+    const $parent = $img.parents('.works__item');
+    const img = { $el: $img, el: $parent[0], played: false };
+    $parent.on('mouseenter', () => {
+      play(img, true);
+      setTimeout(() => $img[0].pauseAnimations(), 750);
+    });
+    $parent.on('mouseleave', () => $img[0].unpauseAnimations());
+    images.push(img);
   });
-})
+});
+const isImgInViewport = () => {
+  const windowTop = window.pageYOffset;
+  const windowBottom = windowTop + window.innerHeight;
+  images.forEach((img) => {
+    if (img.played) return;
+    const top = img.el.offsetTop;
+    const bottom = top + img.el.offsetHeight;
+    if (windowTop < top && windowBottom > bottom) play(img);
+  });
+  window.requestAnimationFrame(isImgInViewport);
+};
+isImgInViewport();
